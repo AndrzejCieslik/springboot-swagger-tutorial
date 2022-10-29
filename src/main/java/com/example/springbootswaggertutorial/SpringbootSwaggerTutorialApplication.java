d278d5620024fcc52e8b9c8e0190758f24764e7f
Andrzej <acieslik.enn@gmail.com>
2022-10-29 20:49:08 +0200

    Initial commit

---
+++ src/main/java/com/example/springbootswaggertutorial/SpringbootSwaggerTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggertutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwaggerTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSwaggerTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootswaggertutorial/SpringbootSwaggerTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswaggertutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwaggerTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
